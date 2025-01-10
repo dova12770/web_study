f09d8769d59511d1d3d8cad22eb6abef6a488fd0
dova12770 <aaaa2460@naver.com>
2025-01-10 15:17:28 +0900

    request 파라미터 전달 연습

--- src/main/java/com/app/controller/QuizController.java
+++ src/main/java/com/app/controller/QuizController.java
@@ -1,8 +1,14 @@
 package com.app.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/quiz")
@@ -22,4 +28,33 @@ public class QuizController {
 		public String keyboard() {
 			return "/quiz/product/keyboard";	
 		}
+		
+		@GetMapping("/req1")
+		public String req1(HttpServletRequest request) {
+			System.out.println(request.getParameter("answer"));
+			return "/quiz/req1";
+		}
+		
+		@PostMapping("/req1")
+		public String req2(HttpServletRequest request) {
+			System.out.println(request.getParameter("answer"));
+			return "/quiz/req1";
+		}
+		
+		@RequestMapping("/req2")
+		public String req3(HttpServletRequest request) {
+			System.out.println(request.getParameter("data1"));
+			System.out.println(request.getParameter("data2"));
+			return "/quiz/req2";
+		}
+		
+		@RequestMapping("/req2")
+		public String req4(@RequestParam Map<String,String> reqMap) {
+			System.out.println("/quiz/req2 요청");
+			System.out.println(reqMap.get("data1"));
+			System.out.println(reqMap.get("data2"));
+			return "/quiz/req2";
+		}
+		
+		
 }

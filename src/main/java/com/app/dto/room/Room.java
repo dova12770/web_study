fa87864f9be846b4909ebd153a8259a525b92fd2
dova12770 <aaaa2460@naver.com>
2025-01-16 15:46:16 +0900

    MVC 구조 Service, DAO계층 형태로 추가

---
+++ src/main/java/com/app/dto/room/Room.java
@@ -0,0 +1,6 @@
+package com.app.dto.room;
+
+public class Room {
+
+	
+}
---
+++ src/main/java/com/app/service/room/RoomService.java
@@ -0,0 +1,11 @@
+package com.app.service.room;
+
+import java.util.List;
+
+import com.app.dto.room.Room;
+
+public interface RoomService {
+	List<Room> findRoomList();//추상메소드
+}
+//impl 구현클래스
+

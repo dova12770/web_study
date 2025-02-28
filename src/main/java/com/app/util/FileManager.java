cb10afbddb6b7893e16c7dc648a3b84c90bd440c
dova12770 <aaaa2460@naver.com>
2025-02-28 14:04:27 +0900

    ?

--- src/main/java/com/app/util/FileManager.java
+++ src/main/java/com/app/util/FileManager.java
@@ -9,34 +9,34 @@ import com.app.dto.file.FileInfo;
 
 public class FileManager {
 
-	static final String FILE_DIRECTORY_PATH = "d:/fileStorage/";
-	static final String FILE_URL_PATH = "/fileStorage/";
-	
-	public static FileInfo storeFile(MultipartFile file) {
-		
-		//파일 폴더 저장 -> 파일에 대한 정보를 기반 -> DB 저장
-		
-		FileInfo fileInfo = new FileInfo();
-		
-		
-		fileInfo.setOriginalFileName(file.getOriginalFilename());
-		fileInfo.setFilePath(FILE_DIRECTORY_PATH);
-		fileInfo.setUrlFilePath(FILE_URL_PATH);
-		
-		Strin extension = extractExtension(file.getOriginalFilename());
-		
-		fileInfo.setFileName(fileName);
-		file.transferTo(new File());
-		return fileInfo;
-	}
-	
-	private String createFileName() {
-		String fileName =UUID.randomUUID().toString();
-		fileName = fileName+","+extension;
-		return fileName;
-	}
-	
-	private String extractExtension() {
-		fileName.substring(0)
-	}
+//	static final String FILE_DIRECTORY_PATH = "d:/fileStorage/";
+//	static final String FILE_URL_PATH = "/fileStorage/";
+//	
+//	public static FileInfo storeFile(MultipartFile file) {
+//		
+//		//파일 폴더 저장 -> 파일에 대한 정보를 기반 -> DB 저장
+//		
+//		FileInfo fileInfo = new FileInfo();
+//		
+//		
+//		fileInfo.setOriginalFileName(file.getOriginalFilename());
+//		fileInfo.setFilePath(FILE_DIRECTORY_PATH);
+//		fileInfo.setUrlFilePath(FILE_URL_PATH);
+//		
+//		String extension = extractExtension(file.getOriginalFilename());
+//		
+//		fileInfo.setFileName(fileName);
+//		file.transferTo(new File());
+//		return fileInfo;
+//	}
+//	
+//	private String createFileName() {
+//		String fileName =UUID.randomUUID().toString();
+//		fileName = fileName+","+extension;
+//		return fileName;
+//	}
+//	
+//	private String extractExtension() {
+//		fileName.substring(0);
+//	}
 }
